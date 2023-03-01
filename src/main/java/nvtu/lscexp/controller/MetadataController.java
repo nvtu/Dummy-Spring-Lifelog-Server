b66cf92e1644ff5d29c0610c04d50ab8a0fa5471
Tu Ninh <tuninh.2411@gmail.com>
2023-03-01 23:56:16 +0000

    🚑 Try enable CORS for one api

--- src/main/java/nvtu/lscexp/controller/MetadataController.java
+++ src/main/java/nvtu/lscexp/controller/MetadataController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class MetadataController {
     @Autowired
     private MetadataService metadataService; 
     
+    @CrossOrigin
     @GetMapping("/date-list")
     public ResponseEntity< List<String> > getDateList() {
         List<String> dateList = this.metadataService.getDateList();
